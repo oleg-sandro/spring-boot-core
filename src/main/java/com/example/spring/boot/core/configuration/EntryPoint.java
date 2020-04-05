803815b528aea8d7e555f7ef6b47e21f89e07dc1
Oleg Sandro <okuziura@gmail.com>
2020-04-06 01:11:16 +0300

    SPBOOT-3 Add endpoint for greeting

--- src/main/java/com/example/spring/boot/core/configuration/EntryPoint.java
+++ src/main/java/com/example/spring/boot/core/configuration/EntryPoint.java
@@ -3,7 +3,7 @@ package com.example.spring.boot.core.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.spring.boot.core.controller")
 public class EntryPoint {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/spring/boot/core/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.spring.boot.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name", defaultValue = "world") String name) {
+        return String.format("Hello %s", name);
+    }
+}
